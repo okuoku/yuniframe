e17ba626e06791c138879c0cc48f354035574632
okuoku <mjt@cltn.org>
2021-04-23 22:54:20 +0900

    cwgl: Implement getParameter

--- cwgl/src-tracker/cwgl-tracker-s6obj.c
+++ cwgl/src-tracker/cwgl-tracker-s6obj.c
@@ -1,41 +1,210 @@
 #include <string.h>
 #include "cwgl-tracker-priv.h"
 
+#define _CASE1(slot, name) case name: *x = ctx->state.slot.name; \
+                                           return CWGL_QR_SUCCESS
+
+#define _CASE2(slot, name) case name: *x = ctx->state.slot.name[0]; \
+                                           *y = ctx->state.slot.name[1]; \
+                                           return CWGL_QR_SUCCESS
+
+#define _CASE4(slot, name) case name: *x = ctx->state.slot.name[0]; \
+                                           *y = ctx->state.slot.name[1]; \
+                                           *z = ctx->state.slot.name[2]; \
+                                           *w = ctx->state.slot.name[3]; \
+                                           return CWGL_QR_SUCCESS
+
 // 6.1.1 Simple Queries
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_b1(cwgl_ctx_t* ctx, cwgl_enum_t pname, int32_t* x){
+#define GLO(x) _CASE1(glo, x)
+    switch(pname){
+        /* GLES2 Table 6.5 */
+        GLO(CULL_FACE);
+        GLO(POLYGON_OFFSET_FILL);
+        /* GLES2 Table 6.6 */
+        GLO(SAMPLE_ALPHA_TO_COVERAGE);
+        GLO(SAMPLE_COVERAGE);
+        GLO(SAMPLE_COVERAGE_INVERT);
+        /* GLES2 Table 6.10 */
+        GLO(SCISSOR_TEST);
+        GLO(STENCIL_TEST);
+        GLO(DEPTH_TEST);
+        /* GLES2 Table 6.11 */
+        GLO(BLEND);
+        GLO(DITHER);
+        /* GLES2 Table 6.12 */
+        GLO(DEPTH_WRITEMASK);
+        /* WebGL1 */
+        GLO(UNPACK_FLIP_Y_WEBGL);
+        GLO(UNPACK_PREMULTIPLY_ALPHA_WEBGL);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_b4(cwgl_ctx_t* ctx, cwgl_enum_t pname, 
                      int32_t* x, int32_t* y, int32_t* z, int32_t* w){
+#define GLO(x) _CASE4(glo, x)
+    switch(pname){
+        GLO(COLOR_WRITEMASK);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_i1(cwgl_ctx_t* ctx, cwgl_enum_t pname, int32_t* x){
+#define CASE(x) _CASE1(cfg, x)
+#define GLO(x) _CASE1(glo, x)
+    switch(pname){
+        /* GLES2 Table 6.18 */
+        CASE(SUBPIXEL_BITS);
+        CASE(MAX_TEXTURE_SIZE);
+        CASE(MAX_CUBE_MAP_TEXTURE_SIZE);
+        CASE(SAMPLE_BUFFERS);
+        CASE(SAMPLES);
+        /* GLES2 Table 6.20 */
+        CASE(MAX_VERTEX_ATTRIBS);
+        CASE(MAX_VERTEX_UNIFORM_VECTORS);
+        CASE(MAX_VARYING_VECTORS);
+        CASE(MAX_COMBINED_TEXTURE_IMAGE_UNITS);
+        CASE(MAX_VERTEX_TEXTURE_IMAGE_UNITS);
+        CASE(MAX_TEXTURE_IMAGE_UNITS);
+        CASE(MAX_FRAGMENT_UNIFORM_VECTORS);
+        CASE(MAX_RENDERBUFFER_SIZE);
+        /* GLES2 Table 6.12 */
+        CASE(RED_BITS);
+        CASE(GREEN_BITS);
+        CASE(BLUE_BITS);
+        CASE(ALPHA_BITS);
+        CASE(DEPTH_BITS);
+        CASE(STENCIL_BITS);
+        /* FIXME: Check framebuffer completion status
+        CASE(IMPLEMENTATION_COLOR_READ_TYPE);
+        CASE(IMPLEMENTATION_COLOR_READ_FORMAT);
+        */
+        /* GLES2 Table 6.5 */
+        GLO(CULL_FACE_MODE);
+        GLO(FRONT_FACE);
+        /* GLES2 Table 6.9 */
+        GLO(ACTIVE_TEXTURE);
+        /* GLES2 Table 6.10 */
+        GLO(STENCIL_FUNC);
+        GLO(STENCIL_VALUE_MASK);
+        GLO(STENCIL_REF);
+        GLO(STENCIL_FAIL);
+        GLO(STENCIL_PASS_DEPTH_FAIL);
+        GLO(STENCIL_PASS_DEPTH_PASS);
+        GLO(STENCIL_BACK_FUNC);
+        GLO(STENCIL_BACK_VALUE_MASK);
+        GLO(STENCIL_BACK_REF);
+        GLO(STENCIL_BACK_FAIL);
+        GLO(STENCIL_BACK_PASS_DEPTH_FAIL);
+        GLO(STENCIL_BACK_PASS_DEPTH_PASS);
+        GLO(DEPTH_FUNC);
+        /* GLES2 Table 6.11 */
+        GLO(BLEND_SRC_RGB);
+        GLO(BLEND_SRC_ALPHA);
+        GLO(BLEND_DST_RGB);
+        GLO(BLEND_DST_ALPHA);
+        GLO(BLEND_EQUATION_RGB);
+        GLO(BLEND_EQUATION_ALPHA);
+        /* GLES2 Table 6.12 */
+        GLO(STENCIL_WRITEMASK);
+        GLO(STENCIL_BACK_WRITEMASK);
+        GLO(STENCIL_CLEAR_VALUE);
+        /* GLES2 Table 6.13 */
+        GLO(UNPACK_ALIGNMENT);
+        GLO(PACK_ALIGNMENT);
+        /* GLES2 Table 6.17 */
+        GLO(GENERATE_MIPMAP_HINT);
+        /* WebGL1 */
+        GLO(UNPACK_COLORSPACE_CONVERSION_WEBGL);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef CASE
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_i2(cwgl_ctx_t* ctx, cwgl_enum_t pname, 
                      int32_t* x, int32_t* y){
+#define CASE(x) _CASE2(cfg, x)
+    switch(pname){
+        CASE(MAX_VIEWPORT_DIMS);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef CASE
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_i4(cwgl_ctx_t* ctx, cwgl_enum_t pname, 
                      int32_t* x, int32_t* y, int32_t* z, int32_t* w){
+#define GLO(x) _CASE2(glo, x)
+    switch(pname){
+        GLO(VIEWPORT);
+        GLO(SCISSOR_BOX);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_f1(cwgl_ctx_t* ctx, cwgl_enum_t pname, float* x){
+#define GLO(x) _CASE1(glo, x)
+    switch(pname){
+        GLO(LINE_WIDTH);
+        GLO(POLYGON_OFFSET_FACTOR);
+        GLO(POLYGON_OFFSET_UNITS);
+        GLO(SAMPLE_COVERAGE_VALUE);
+        GLO(DEPTH_CLEAR_VALUE);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_f2(cwgl_ctx_t* ctx, cwgl_enum_t pname, float* x, float* y){
+#define CASE(x) _CASE2(cfg, x)
+#define GLO(x) _CASE2(glo, x)
+    switch(pname){
+        CASE(ALIASED_POINT_SIZE_RANGE);
+        CASE(ALIASED_LINE_WIDTH_RANGE);
+        GLO(DEPTH_RANGE);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef CASE
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
 cwgl_getParameter_f4(cwgl_ctx_t* ctx, cwgl_enum_t pname, 
                      float* x, float* y, float* z, float* w){
+#define GLO(x) _CASE4(glo, x)
+    switch(pname){
+        GLO(BLEND_COLOR);
+        GLO(COLOR_CLEAR_VALUE);
+        default:
+        CTX_SET_ERROR(ctx, INVALID_OPERATION);
+        return CWGL_QR_GLERROR;
+    }
+#undef GLO
 }
 
 CWGL_API cwgl_query_result_t 
