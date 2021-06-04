c2b4b17df774932d5e16dfd44957f5bcfa83f2ac
okuoku <mjt@cltn.org>
2021-06-05 06:08:14 +0900

    cwgl gles2: Add cfg init

---
+++ cwgl/src-tgles2/cwgl-backend-states.h
@@ -0,0 +1,16 @@
+#ifndef __YUNI_CWGL_BACKEND_GLES2_STATES_H
+#define __YUNI_CWGL_BACKEND_GLES2_STATES_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+// }
+
+int cwgl_backend_cfg_init(cwgl_ctx_config_t* cfg);
+
+// {
+#ifdef __cplusplus
+};
+#endif
+
+#endif
