233af43a3577adc215e82365c1b468a82819c6be
okuoku <mjt@cltn.org>
2021-07-27 20:53:41 +0900

    sdl2: Add Vulkan window init

--- platform/src-sdl2/yfrm-ctx-sdl2.c
+++ platform/src-sdl2/yfrm-ctx-sdl2.c
@@ -84,6 +84,46 @@ struct cwgl_ctx_s {
 };
 #endif
 
+#ifdef CWGL_EXPERIMENTAL_TRACKER
+static cwgl_ctx_t*
+ctx_create_VK(int32_t width, int32_t height, int32_t reserved,
+                     int32_t flags){
+    SDL_GLContext glc;
+    cwgl_ctx_t* r;
+    cwgl_platform_ctx_t* p;
+
+    if(! wnd){
+        SDL_Window* window;
+        /* Init SDL and Create a window */
+        if(SDL_Init(SDL_INIT_VIDEO|SDL_INIT_GAMECONTROLLER|SDL_INIT_AUDIO|SDL_INIT_TIMER)){
+            printf("SDL Init failed.\n");
+            return NULL;
+        }
+
+        if(!(window = SDL_CreateWindow("cwgl",
+                                       SDL_WINDOWPOS_UNDEFINED,
+                                       SDL_WINDOWPOS_UNDEFINED,
+                                       width, height,
+                                       0))){
+            SDL_Quit();
+            printf("SDL CreateWindow failed.\n");
+            return NULL;
+        }
+        wnd = window;
+        // FIXME: Create surface with SDL_Vulkan_CreateSurface here
+    }
+
+    r = malloc(sizeof(cwgl_ctx_t));
+    cwgl_integ_ctx_init(r);
+    p = malloc(sizeof(cwgl_platform_ctx_t));
+    p->wnd = wnd;
+    //p->glc = glc;
+    r->platform = p;
+
+    return r;
+}
+#endif
+
 static cwgl_ctx_t*
 ctx_create_EGL(int32_t width, int32_t height, int32_t reserved,
                      int32_t flags){
@@ -224,7 +264,9 @@ ctx_create_ANGLE(int32_t width, int32_t height, int32_t reserved,
 YFRM_API cwgl_ctx_t*
 yfrm_cwgl_ctx_create(int32_t width, int32_t height, int32_t reserved,
                      int32_t flags){
-#ifndef YFRM_CWGL_USE_ANGLE
+#if defined(CWGL_EXPERIMENTAL_TRACKER) && defined(YFRM_CWGL_USE_VULKAN)
+    return ctx_create_VK(width, height, reserved, flags);
+#elif !defined(YFRM_CWGL_USE_ANGLE)
     return ctx_create_EGL(width, height, reserved, flags);
 #else
     return ctx_create_ANGLE(width, height, reserved, flags);
