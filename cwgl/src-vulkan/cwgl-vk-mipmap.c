a9963f2ca99f0970009e39881820073775dc07bd
okuoku <mjt@cltn.org>
2023-07-09 19:19:04 +0900

    cwgl: Move to dedicated repository
    Moved to https://github.com/okuoku/cwgl

--- cwgl/src-vulkan/cwgl-vk-mipmap.c
+++
@@ -1,8 +0,0 @@
-#include "cwgl-backend-vk-priv.h"
-#include <stdio.h>
-
-int
-cwgl_backend_generateMipmap(cwgl_ctx_t* ctx, cwgl_enum_t target){
-    printf("UNIMPL: generateMipmap\n");
-    return 0;
-}
