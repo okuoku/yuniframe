a1972acbc72251e4ca5d0566a67a385542fea504
okuoku <mjt@cltn.org>
2023-07-18 22:05:14 +0900

    Add nccc stub

---
+++ nccc/nccc_cwgl.c
@@ -0,0 +1,4 @@
+#include <yuniframe/cwgl.h>
+#define STUBNAME cwgl
+#include "stubdata_cwgl.h"
+#include "stubcode.inc.c"
---
+++ nccc/nccc_yfrm.c
@@ -0,0 +1,4 @@
+#include <yuniframe/yfrm.h>
+#define STUBNAME yfrm
+#include "stubdata_yfrm.h"
+#include "stubcode.inc.c"
