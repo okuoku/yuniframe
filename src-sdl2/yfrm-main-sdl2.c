08273cc853714dbdcf4647b1274f3c0925b8044f
okuoku <mjt@cltn.org>
2024-12-01 13:09:51 +0900

    main: Split `ENTRYPOINT` and `FRAME`

--- src-sdl2/yfrm-main-sdl2.c
+++ src-sdl2/yfrm-main-sdl2.c
@@ -1,9 +1,40 @@
 #include "SDL2/SDL_main.h"
 
-int YFRM_ENTRYPOINT(int ac, const char** av);
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
+
+int YFRM_ENTRYPOINT(int ac, char** av);
+int YFRM_FRAME(void* bogus);
+
+#ifdef __EMSCRIPTEN__
+static void
+loop(void){
+    int r;
+    r = YFRM_FRAME(0);
+    if(r){
+        emscripten_cancel_main_loop();
+    }
+}
+#endif
 
 int
 main(int ac, char** av){
-    return YFRM_ENTRYPOINT(ac, av);
+    int r;
+    r = YFRM_ENTRYPOINT(ac, av);
+    if(r){
+        return r;
+    }
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(loop, 0, 1);
+#else
+    for(;;){
+        r = YFRM_FRAME(0);
+        if(r){
+            return r;
+        }
+    }
+#endif
 }
+
 
--- src-stdc/yfrm-main-stdc.c
+++ src-stdc/yfrm-main-stdc.c
@@ -1,7 +1,39 @@
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#include <emscripten/html5.h>
+#endif
+    
 int YFRM_ENTRYPOINT(int ac, const char** av);
+int YFRM_FRAME(void* bogus);
+    
+#ifdef __EMSCRIPTEN__
+static void
+loop(void){
+    int r;
+    r = YFRM_FRAME(0);
+    if(r){
+        emscripten_cancel_main_loop();
+    }
+}   
+#endif
 
 int
-main(int ac, char** av){
-    return YFRM_ENTRYPOINT(ac, av);
+main(int ac, const char** av){
+    int r;
+    r = YFRM_ENTRYPOINT(ac, av);
+    if(r){
+        return r;
+    }
+#ifdef __EMSCRIPTEN__
+    emscripten_set_canvas_element_size("#canvas", 1280, 720);
+    emscripten_set_main_loop(loop, 0, 1);
+#else
+    for(;;){
+        r = YFRM_FRAME(0);
+        if(r){
+            return r;
+        }
+    }
+#endif
 }
 
